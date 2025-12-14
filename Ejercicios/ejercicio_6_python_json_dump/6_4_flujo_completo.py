e5e6c73e603e63a34e6566e3358772a1bc5df3ab
Aisha del Tio <adeltiodeprado@gmail.com>
2025-12-14 11:43:00 +0100

    ejercicio 6 - mostrar todos los diccionarios reconstruidos

--- Ejercicios/ejercicio_6_python_json_dump/6_4_flujo_completo.py
+++ Ejercicios/ejercicio_6_python_json_dump/6_4_flujo_completo.py
@@ -81,3 +81,49 @@ for e in range(5):
     recibido_dict = json.loads(recibido_str)
     print("\nDiccionario reconstruido:", recibido_dict)
     print("Tipo:", type(recibido_dict))  # dict
+    
+    
+# 5. Muestra todas las iteraciones, no solo la última.
+todos_los_diccionarios = []
+for e in range(2):
+    # PASO 1: Creamos el diccionario con datos simulados
+    print(f"\n=== Iteración {e+1} ===")
+    data = {
+        'Proyecto': f'Innovación #{e+1}',
+        'Presupuesto': f'{(e+1)*5000} EUR',
+        'Estado': 'Aprobado 😊' if e % 2 == 0 else 'En revisión 🥹',
+        'Fecha': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),  # Fecha actual
+        'Temperatura_C': 95.6,          # Temperatura en grados Celsius
+        'Presion_Barra': 2.3,           # Presión en bares
+        'RPM': 7200,                    # Revoluciones por minuto
+        'Sensores': ['Temperatura', 'Presión', 'RPM']  # Lista de sensores activos
+    }
+    print("\nDiccionario original:", data)
+    print("Tipo:", type(data))  # dict
+
+    # PASO 2: Convertimos el diccionario a JSON (texto)
+    data_str = json.dumps(data)
+    print("\nTexto JSON:", data_str)
+    print("Tipo:", type(data_str))  # str
+
+    # PASO 3: Convertimos el texto a bytes (para enviar por red)
+    data_bytes = data_str.encode('utf-8')
+    print("\nBytes (para Kafka/red):", data_bytes)
+    print("Tipo:", type(data_bytes))  # bytes
+
+    # PASO 4: Simulamos recepción y decodificación (bytes -> texto)
+    recibido_str = data_bytes.decode('utf-8')
+    print("\nTexto decodificado:", recibido_str)
+    print("Tipo:", type(recibido_str))  # str
+
+    # PASO 5: Convertimos el texto decodificado de nuevo a diccionario
+    recibido_dict = json.loads(recibido_str)
+    print("\nDiccionario reconstruido:", recibido_dict)
+    print("Tipo:", type(recibido_dict))  # dict
+    
+    # PASO 6: Guardar diccionario reconstruido
+    todos_los_diccionarios.append(recibido_dict)
+    
+print("Imprimir todos los diccionarios reconstruidos")
+for dic in todos_los_diccionarios:
+    print(dic)
